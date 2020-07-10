195cc8e83942c566a3036c73d099e6b335e146c9
Valdir Silva <valdirpiresba@gmail.com>
2020-07-09 23:26:04 -0300

    Criando uma rota que recebe dois numeros inteiros e retorna a soma

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    resultado = int(num1) + int(num2)
+    return HttpResponse(resultado)
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -19,5 +19,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<idade>/', views.hello)
+    path('hello/<nome>/<idade>/', views.hello),
+    path('soma/<num1>/<num2>/', views.soma)
 ]
